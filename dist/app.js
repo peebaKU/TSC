2522330e8865627cc64fb8e0484fe18fc8bdfc0b
Peeba <baramee.254450@gmail.com>
2023-04-30 15:06:21 +0700

    first commit

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let fname = "Baramee";
+let age = 20;
+let isWorking = false;
+console.log(`My name is ${fname.toUpperCase()} and my age is ${age} and I'm working is ${(isWorking) ? 'Yes' : 'No'}`);
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+let fname:string = "Baramee";
+let age:number = 20;
+let isWorking:boolean = false;
+
+console.log(`My name is ${fname.toUpperCase()} and my age is ${age} and I'm working is ${(isWorking)?'Yes':'No'}`);
